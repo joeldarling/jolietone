c01e98c2168ced4313d021f41f72c0d9fa31ea64
Joel Darling <joel.darling@gmail.com>
2016-05-02 14:27:11 -0400

    connected UI to synth

--- js/app.js
+++ js/app.js
@@ -12,9 +12,39 @@ var monoSynth = new Tone.MonoSynth({
     }
 }).toMaster();
 
+Tone.Master.volume.rampTo(0, 0.05);
 
-window.onload = function() {
-  monoSynth.triggerAttackRelease("C2", "8n");
-  Tone.Master.volume.rampTo(0, 0.05);
+nx.onload = function() {
+  nx.colorize("#00CCFF"); // sets accent (default)
 
+
+  attack.on("*", function(data){
+    monoSynth.filterEnvelope.attack = data.value;
+  });
+  decay.on("*", function(data){
+    monoSynth.filterEnvelope.decay = data.value;
+  });
+  sustain.on("*", function(data){
+    monoSynth.filterEnvelope.sustain = data.value;
+  });
+  release.on("*", function(data){
+    monoSynth.filterEnvelope.release = data.value;
+  });
+  keyboard.on("*", function(data){
+    console.log(convertMIDI(data.note))
+    if(data.on > 0)
+      monoSynth.triggerAttackRelease(convertMIDI(data.note), "4n");
+
+  });
+
+
+};
+
+var convertMIDI = function(noteNum){
+
+  var octave = Math.floor((noteNum / 12) - 1);
+  var note = "C C#D D#E F F#G G#A A#B ".substr((noteNum % 12) * 2, 2);
+  var result = note+octave;
+
+  return result.replace(/\s/g, '');
 };
