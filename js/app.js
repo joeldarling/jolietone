e3254827525a4222eb5fe1d53c44f92d06a4b6dc
Joel Darling <joel.darling@gmail.com>
2016-05-04 15:23:47 -0400

    v1 done

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ var monoSynth = new Tone.MonoSynth({
         "sustain" : 0.2,
         "release" : 0.9,
     },
+    filter: {
+      type: 'lowpass'
+    },
     oscillator:{
       type:"sine"
     }
@@ -16,54 +19,33 @@ var monoSynth = new Tone.MonoSynth({
 /// INIT FX ///
 var pingPong = new Tone.PingPongDelay("16n", 0.5).toMaster();
 var freeverb = new Tone.Freeverb({ "wet": 0 }).toMaster();
-var phaser = new Tone.Phaser({
-	"frequency" : 150,
-	"octaves" : 5,
-	"baseFrequency" : 1000
-}).toMaster();
-var autoWah = new Tone.AutoWah(50, 6, -30).toMaster();
+var distort = new Tone.Distortion(0.8).toMaster();
+
+
 
 /// connect to synth ///
 monoSynth.connect(pingPong);
 monoSynth.connect(freeverb);
-monoSynth.connect(autoWah);
+monoSynth.connect(distort);
 
-
-Tone.Master.volume.rampTo(0, 0.05);
-console.log(monoSynth.filter.get());
-console.log(monoSynth.filter.get());
+//set initial volume
+Tone.Master.volume.rampTo(-10, 0.05);
 
 nx.onload = function() {
-  nx.colorize("#00CCFF"); // sets accent (default)
+  nx.colorize("#212121"); // sets accent (default)
 
   initControls();
 
   //event listeners
-  /// FLT ///
-  chrs.on("*", function(data){
-
-
-  });
-
-  /// RVB ///
+  /// VCO ///
   tabs1.on("*", function(data){
 
+    if(data.text==='saw')
+      data.text = 'sawtooth';
     monoSynth.oscillator.set({type: data.text});
 
   });
 
-
-  /// RVB ///
-  rvbAmt.on("*", function(data){
-    freeverb.set({wet: data.value});
-
-  });
-
-  /// RVB ///
-  dlyAmt.on("*", function(data){
-    pingPong.set({wet: +data.value});
-
-  });
   /// ADSR ///
   attack.on("*", function(data){
     monoSynth.envelope.attack = data.value;
@@ -77,6 +59,43 @@ nx.onload = function() {
   release.on("*", function(data){
     monoSynth.envelope.release = data.value;
   });
+
+  /// FLTR ///
+  dist.on("*", function(data){
+
+    if (+data.value === 1) {
+      console.log('enable', distort.get());
+      distort.set({wet: 1});
+      console.log('enable', distort.get());
+
+    } else{
+      console.log('disnable', distort.get());
+
+      distort.set({wet: 0});
+      console.log('disnable', distort.get());
+
+
+    }
+  });
+
+  /// RVB ///
+  rvbAmt.on("*", function(data){
+    freeverb.set({wet: data.value});
+
+  });
+
+  /// DLY ///
+  dlyAmt.on("*", function(data){
+    pingPong.set({wet: data.value});
+
+  });
+
+  /// VCA ///
+  volume.on("*", function(data){
+    Tone.Master.volume.rampTo(+data.value, 0.05);
+  });
+
+  //KBD
   keyboard.on("*", function(data){
     if(data.on > 0)
       monoSynth.triggerAttack(convertMIDI(data.note));
@@ -90,13 +109,16 @@ nx.onload = function() {
 var initControls = function(){
 
   //VCO
-  tabs1.options = ['sine','sawtooth','square'];
-  tabs1.choice = 2;
+  tabs1.options = ['sine','saw','square'];
+  tabs1.choice = 0;
   tabs1.init();
   tabs1.draw();
 
   //fltr
-  chrs.set({value:0});
+  distort.set("wet", 0);
+
+  //VCA
+  volume.set({value:-10, label: false});
 
   //ADSR
   attack.set({value:0.02});
