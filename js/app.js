cb7200dacf2641beb82926c90ffc34c39e02af69
Joel Darling <joel.darling@gmail.com>
2016-05-11 14:29:42 -0400

    adding midi suppport

--- js/app.js
+++ js/app.js
@@ -1,3 +1,53 @@
+/// SETUP MIDI ///
+
+// request MIDI access
+if (navigator.requestMIDIAccess) {
+    navigator.requestMIDIAccess({
+        sysex: false // this defaults to 'false' and we won't be covering sysex in this article.
+    }).then(onMIDISuccess, onMIDIFailure);
+} else {
+    alert("No MIDI support in your browser.");
+}
+
+// midi functions
+function onMIDISuccess(midiAccess) {
+  // when we get a succesful response, run this code
+  var midi = midiAccess; // this is our raw MIDI data, inputs, outputs, and sysex status
+
+  var inputs = midi.inputs.values();
+  // loop over all available inputs and listen for any MIDI input
+  for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
+      // each time there is a midi message call the onMIDIMessage function
+      input.value.onmidimessage = onMIDIMessage;
+      console.log(input)
+      listInputs(input);
+
+  }
+
+  midi.onstatechange = onStateChange;
+
+}
+
+function listInputs(inputs) {
+    var input = inputs.value;
+    console.log("Input port : [ type:'" + input.type + "' id: '" + input.id +
+        "' manufacturer: '" + input.manufacturer + "' name: '" + input.name +
+        "' version: '" + input.version + "']");
+}
+
+function onStateChange(message){
+  console.log('message')
+}
+
+function onMIDIMessage(message) {
+    var data = message.data; // this gives us our [command/channel, note, velocity] data.
+    console.log('MIDI data', data); // MIDI data [144, 63, 73]
+}
+
+function onMIDIFailure(e) {
+    // when we get a failed response, run this code
+    console.log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + e);
+}
 
 /// SETUP OSC ///
 var monoSynth = new Tone.MonoSynth({
@@ -129,6 +179,7 @@ var initControls = function(){
   pingPong.set("wet",0);
 
 };
+
 var convertMIDI = function(noteNum){
 
   var octave = Math.floor((noteNum / 12) - 1);
